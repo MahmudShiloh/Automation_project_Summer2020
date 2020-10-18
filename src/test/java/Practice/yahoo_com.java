bb1791f84f2c6bcd2044762b282809fd64712490
MahmudShiloh <Mahmudshiloh@gmail.com>
2020-10-18 11:37:16 -0400

    yahoo action item

---
+++ src/test/java/Practice/yahoo_com.java
@@ -0,0 +1,32 @@
+package Practice;
+
+import Reusable_Library.Abstract_Class;
+import Reusable_Library.Reusable_Methods;
+import Reusable_Library.Reusable_Methods_Loggers;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.annotations.Test;
+
+public class yahoo_com extends Reusable_Methods_Loggers {
+
+
+    Test@
+    public void searchKeyword{
+        logger.log(LogStatus.INFO,("https://www.mortgagecalculator.org");
+
+        //navigate to yahoo com
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
